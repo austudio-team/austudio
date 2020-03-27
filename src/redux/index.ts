67fc53db5d8c304c9359c166a9438a3daeb4688f
delbertbeta <delbertbeta@live.com>
2020-03-27 22:03:30 +0800

    fix: fix blank screen on browser with no redux devtool

--- src/redux/index.ts
+++ src/redux/index.ts
@@ -7,12 +7,16 @@ const sagaMiddleware = createSagaMiddleware();
 
 const store = createStore(
   rootReducer,
-  compose(
+  (window as any).__REDUX_DEVTOOLS_EXTENSION__ ?
+    compose(
+      applyMiddleware(
+        sagaMiddleware,
+      ),
+      (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
+    ) :
     applyMiddleware(
       sagaMiddleware,
     ),
-    (window as any).__REDUX_DEVTOOLS_EXTENSION__ && (window as any).__REDUX_DEVTOOLS_EXTENSION__(),
-  ),
 );
 
 sagaMiddleware.run(rootSaga);
